fd0adcc4a62da9a03c98c63061d7d5e7b7849aff
katsenelenbogen <73099925+katsenelenbogen@users.noreply.github.com>
2022-10-28 17:34:54 +0300

    Add files via upload

---
+++ testsslTOxlsx/README.md
@@ -0,0 +1,84 @@
+<p align="center">
+  <img alt="logo" src="images/testssl2xlsx.png" height="200">
+  <h1 align="center">
+    testssl2xlsx
+  </h1>
+  <p align="center">
+      <a href="https://www.python.org"><img alt="lang" src="https://img.shields.io/badge/Lang-Python-blue.svg"></a>
+      <a href="https://opensource.org/licenses/Apache-2.0"><img alt="license" src="https://img.shields.io/badge/License-Apache%202.0-red.svg"></a>
+      <br>
+      <img alt="bitcoin" src="https://img.shields.io/badge/Bitcoin-15aFaQaW9cxa4tRocax349JJ7RKyj7YV1p-yellow.svg">
+      <img alt="bitcoin cash" src="https://img.shields.io/badge/Bitcoin%20Cash-qqez5ed5wjpwq9znyuhd2hdg86nquqpjcgkm3t8mg3-yellow.svg">
+      <img alt="ether" src="https://img.shields.io/badge/Ether-0x70bC178EC44500C17B554E62BC31EA2B6251f64B-yellow.svg">
+  </p>
+</p>
+
+# This project has now been deprecated. Its functionality has been incorporated into [pentest2xlsx](https://github.com/AresS31/pentest2xlsx).
+
+Parse `testssl` scans results (in a `JSON` format) into `Excel` tables to facilitate the reporting process of penetration tests, especially useful when dealing with large scopes. Having scans results organised in `Excel` tables also allow customers and testers to use `Excel` powerful filtering capabilities.
+
+The following Excel worksheets are generated:
+* `Host vs Certificate`
+* `Host vs Certificates`
+* `Host vs Protocol`
+* `Host vs Protocols`
+* `Host vs Vulnerability`
+* `Host vs Vulnerabilities`
+
+## Installation
+```
+$ git clone https://github.com/AresS31/testssl2xlsx
+# python -m pip install -r testssl2xlsx/requirements.txt
+ ```
+
+## Usage
+```
+testssl2xlsx.py [-h] -iJ INPUT_FILES [INPUT_FILES ...]
+                [-oX OUTPUT_FILE] [-v]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -iJ INPUT_FILES [INPUT_FILES ...], --input-json INPUT_FILES [INPUT_FILES ...]
+                        input from testssl file(s) in JSON format
+  -oX OUTPUT_FILE, --output-xlsx OUTPUT_FILE
+                        output results to a specified <OUTPUT_FILE> in XLSX
+                        format
+  -v, --verbose         increase verbosity level
+```
+
+## Example
+1. Generate the `testssl` input file with:
+
+`$ testssl --jsonfile-pretty testssl-results.json --quiet --nodns none --parallel --server-defaults --protocols --vulnerable --file ../nmap/SYN-*.gnmap`
+
+2. Run `testssl2xlsx` with the testssl `JSON` file generated in the previous step:
+
+`$ python testssl2xlsx.py -iJ testssl-results.json`
+
+## Possible Improvements
+- [ ] Adding new features, such as --protocols filtering.
+- [ ] Enforce the correct file extensions (input file(s) must be .json, output file must be .xlsx)
+- [ ] Reconfigure the logging module to load its settings with a configuration file
+- [ ] Implement support for directory input rather than individual files.
+- [ ] Source code optimisation.
+
+## Donations
+* Via Bitcoin      : **15aFaQaW9cxa4tRocax349JJ7RKyj7YV1p**
+* Via Bitcoin Cash : **qqez5ed5wjpwq9znyuhd2hdg86nquqpjcgkm3t8mg3**
+* Via Ether        : **0x70bC178EC44500C17B554E62BC31EA2B6251f64B**
+
+## License(s)
+### testssl2xlsx
+Copyright 2017 - 2019 Alexandre Teyar
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  <http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
---
+++ testsslTOxlsx/testsslTOxlsx.py
@@ -0,0 +1,487 @@
+#!/usr/bin/env python3
+#    Copyright 2017 - 2019 Alexandre Teyar
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this output_file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import argparse
+import json
+import logging
+import sys
+import time
+import xlsxwriter
+
+# add or remove entries from the lists below in order to enable/disable
+# reporting for the selected entries - respect the case -
+certificates = {
+    "cert_chain_of_trust": {
+        "name": "Chain of Trust"
+    },
+    "cert_expiration_status": {
+        "name": "Expired"
+    },
+    "cert_signatureAlgorithm": {
+        "name": "Weak Hashing Algorithm"
+    },
+    "cert_trust": {
+        "name": "Trust"
+    }
+}
+
+protocols = sorted([
+    "SSLv2",
+    "SSLv3",
+    "TLS1",
+    "TLS1_1",
+    "TLS1_2",
+    "TLS1_3"
+])
+
+ciphers = sorted([
+    "cipherlist_NULL",
+    "cipherlist_aNULL",
+    "cipherlist_EXPORT",
+    "cipherlist_LOW",
+    "cipherlist_3DES_IDEA",
+    "cipherlist_AVERAGE",
+    "cipherlist_STRONG"
+])
+
+
+vulnerabilities = {
+    "BEAST": {
+        "name": "BEAST"
+    },
+    "fallback_SCSV": {
+        "name": "Fallback SCSV"
+    },
+    "CCS": {
+        "name": "CCS"
+    },
+    "BREACH": {
+        "name": "BREACH"
+    },
+    "CRIME_TLS": {
+        "name": "CRIME"
+    },
+    "heartbleed": {
+        "name": "heartbleed"
+    },
+    "FREAK": {
+        "name": "FREAK"
+    },
+    "LOGJAM-common_primes": {
+        "name": "Logjam Common Primes"
+    },
+    "LOGJAM": {
+        "name": "Logjam"
+    },
+    "LUCKY13": {
+        "name": "Lucky13"
+    },
+    "POODLE_SSL": {
+        "name": "POODLE"
+    },
+    "RC4": {
+        "name": "RC4"
+    },
+    "ROBOT": {
+        "name": "ROBOT"
+    },
+    "secure_client_renego": {
+        "name": "Secure Client Renegotiation"
+    },
+    "SWEET32": {
+        "name": "Sweet32"
+    },
+    "DROWN": {
+        "name": "Drown"
+    }
+}
+
+
+def parse_args():
+    """ Parse and validate the command line
+    """
+    parser = argparse.ArgumentParser(
+        description=(
+            "Parse testssl results into an Excel spreadsheet"
+        )
+    )
+
+    parser.add_argument(
+        "-iJ",
+        "--input-json",
+        dest="input_files",
+        help="input from testssl file(s) in JSON format",
+        nargs='+',
+        required=True,
+        type=argparse.FileType('r')
+    )
+
+    parser.add_argument(
+        "-oX",
+        "--output-xlsx",
+        default="testssl-results_{}.xlsx".format(
+            time.strftime("%Y%m%d-%H%M%S")),
+        dest="output_file",
+        help="output results to a specified <OUTPUT_FILE> in XLSX format",
+        required=False,
+        type=str
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_const",
+        const=logging.DEBUG,
+        default=logging.INFO,
+        dest="loglevel",
+        help="increase verbosity level",
+        required=False
+    )
+
+    return parser.parse_args()
+
+
+def insert_at_index(headers, d):
+    """ insert values at the appropriate index
+    """
+    data = ["N/A"] * len(headers)
+
+    for key, values in d.items():
+        if isinstance(values, dict):
+            data[headers.index(values["name"])] = values.get("severity")
+        else:
+            data[headers.index(key)] = values
+
+    return data
+
+
+def draw_table(worksheet, table_headers, table_data):
+    """ Create an Excel worksheet containing the 'table_headers'
+        and 'table_data' dataset
+    """
+    column_count = 0
+    row_count = 0
+    table_column_count = len(table_headers) - 1
+    table_row_count = len(table_data)
+
+    logging.debug("{}".format(table_headers))
+    logging.debug("{}".format(table_data))
+
+    worksheet.add_table(
+        row_count,
+        column_count,
+        table_row_count,
+        table_column_count,
+        {
+            "banded_rows": True,
+            "columns": table_headers,
+            "data": table_data,
+            "first_column": True,
+            "style": "Table Style Medium 1"
+        }
+    )
+
+
+def parse_host_certificate(workbook, data):
+    table_data = []
+    table_headers = [
+        {"header": "Host IP"},
+        {"header": "Port"},
+        {"header": "Vulnerability"},
+        {"header": "Severity"},
+        {"header": "Information"}
+    ]
+
+    try:
+        for entry in data:
+            for values in entry["scanResult"]:
+                for serverDefault in values["serverDefaults"]:
+                    if serverDefault["id"] in certificates.keys():
+                        table_data.append(
+                            [
+                                values["ip"],
+                                int(values["port"]),
+                                certificates[serverDefault["id"]]["name"],
+                                serverDefault["severity"],
+                                serverDefault["finding"]
+                            ]
+                        )
+
+        worksheet = workbook.add_worksheet("Host vs Certificate")
+        draw_table(worksheet, table_headers, table_data)
+    except KeyError as e:
+        logging.exception("KeyError - {}".format(e))
+
+
+def parse_host_certificates(workbook, data):
+    table_data = []
+    table_headers = [
+        {"header": "Host IP"},
+        {"header": "Port"}
+    ]
+
+    for values in certificates.values():
+        table_headers.append({"header": values["name"]})
+
+    try:
+        for entry in data:
+            for values in entry["scanResult"]:
+                d = {
+                    "Host IP": values["ip"],
+                    "Port": int(values["port"])
+                }
+
+                for serverDefault in values["serverDefaults"]:
+                    if serverDefault["id"] in certificates.keys():
+                        d[serverDefault["id"]] = {
+                            "name": certificates[serverDefault["id"]]["name"],
+                            "severity": serverDefault["severity"]
+                        }
+
+                table_data.append(
+                    insert_at_index([x["header"] for x in table_headers], d))
+
+        worksheet = workbook.add_worksheet("Host vs Certificates")
+        draw_table(worksheet, table_headers, table_data)
+    except KeyError as e:
+        logging.exception("KeyError - {}".format(e))
+
+
+def parse_host_protocol(workbook, data):
+    table_data = []
+    table_headers = [
+        {"header": "Host IP"},
+        {"header": "Port"},
+        {"header": "Supported Protocol"},
+        {"header": "Severity"}
+    ]
+
+    try:
+        for entry in data:
+            for values in entry["scanResult"]:
+                for protocol in values["protocols"]:
+                    if protocol["id"] in protocols:
+                        if protocol["finding"].split(' ', 1)[0] == "offered" or protocol["finding"].split(' ', 1)[0] == "vulnerable":
+                            table_data.append(
+                                [
+                                    values["ip"],
+                                    int(values["port"]),
+                                    protocol["id"],
+                                    protocol["severity"]
+                                ]
+                            )
+
+        worksheet = workbook.add_worksheet("Host vs Protocol")
+        draw_table(worksheet, table_headers, table_data)
+    except KeyError as e:
+        logging.exception("KeyError - {}".format(e))
+
+
+def parse_host_protocols(workbook, data):
+    table_data = []
+    table_headers = [
+        {"header": "Host IP"},
+        {"header": "Port"}
+    ]
+
+    for protocol in protocols:
+        table_headers.append({"header": protocol})
+
+    try:
+        for entry in data:
+            for values in entry["scanResult"]:
+                d = {
+                    "Host IP": values["ip"],
+                    "Port": int(values["port"])
+                }
+
+                for protocol in values["protocols"]:
+                    if protocol["id"] in protocols:
+                        if protocol["finding"].split(' ', 1)[0] == "offered" or protocol["finding"].split(' ', 1)[0] == "vulnerable":
+                            d[protocol["id"]] = protocol["severity"]
+                        else:
+                            d[protocol["id"]] = "NO"
+
+                table_data.append(
+                    insert_at_index([x["header"] for x in table_headers], d))
+
+        worksheet = workbook.add_worksheet("Host vs Protocols")
+        draw_table(worksheet, table_headers, table_data)
+    except KeyError as e:
+        logging.exception("KeyError - {}".format(e))
+
+
+def parse_host_ciphers(workbook, data):
+    table_data = []
+    table_headers = [
+        {"header": "Host IP"},
+        {"header": "Port"}
+    ]
+
+    for cipher in ciphers:
+        table_headers.append({"header": cipher})
+
+    try:
+        for entry in data:
+            for values in entry["scanResult"]:
+                d = {
+                    "Host IP": values["ip"],
+                    "Port": int(values["port"])
+                }
+
+                for cipher in values["ciphers"]:
+                    if cipher["id"] in ciphers:
+                        if cipher["severity"] != "OK":
+                            d[cipher["id"]] = cipher["severity"]
+                        else:
+                            d[cipher["id"]] = "NO"
+
+                table_data.append(
+                    insert_at_index([x["header"] for x in table_headers], d))
+
+        worksheet = workbook.add_worksheet("Host vs Ciphers")
+        draw_table(worksheet, table_headers, table_data)
+    except KeyError as e:
+        logging.exception("KeyError - {}".format(e))
+
+
+def parse_host_vulnerability(workbook, data):
+    table_data = []
+    table_headers = [
+        {"header": "Host IP"},
+        {"header": "Port"},
+        {"header": "Vulnerability"},
+        {"header": "Severity"},
+        {"header": "CVE"},
+        {"header": "Information"}
+    ]
+
+    try:
+        for entry in data:
+            for values in entry["scanResult"]:
+                for vulnerability in values["vulnerabilities"]:
+                    if vulnerability["id"] in vulnerabilities.keys():
+                        table_data.append(
+                            [
+                                values["ip"],
+                                int(values["port"]),
+                                vulnerabilities[vulnerability["id"]]["name"],
+                                vulnerability["severity"],
+                                # avoid to raise KeyError exceptions for the
+                                # entries without CVE number and replace space
+                                # with a comma
+                                vulnerability.get("cve", "N/A")
+                                .replace(" ", ", "),
+                                vulnerability["finding"]
+                            ]
+                        )
+
+        worksheet = workbook.add_worksheet("Host vs Vulnerability")
+        draw_table(worksheet, table_headers, table_data)
+    except KeyError as e:
+        logging.exception("KeyError - {}".format(e))
+
+
+def parse_host_vulnerabilities(workbook, data):
+    table_data = []
+    table_headers = [
+        {"header": "Host IP"},
+        {"header": "Port"}
+    ]
+
+    for values in vulnerabilities.values():
+        table_headers.append({"header": values["name"]})
+
+    try:
+        for entry in data:
+            for values in entry["scanResult"]:
+                d = {
+                    "Host IP": values["ip"],
+                    "Port": int(values["port"])
+                }
+
+                for vulnerability in values["vulnerabilities"]:
+                    if vulnerability["id"] in vulnerabilities.keys():
+                        d[vulnerability["id"]] = {
+                            "name": vulnerabilities[vulnerability["id"]]
+                            ["name"],
+                            "severity": vulnerability["severity"]
+                        }
+
+                table_data.append(
+                    insert_at_index([x["header"] for x in table_headers], d))
+
+        worksheet = workbook.add_worksheet("Host vs Vulnerabilities")
+        draw_table(worksheet, table_headers, table_data)
+    except KeyError as e:
+        logging.exception("KeyError - {}".format(e))
+
+
+def main():
+    args = parse_args()
+
+    logging.basicConfig(
+        format="%(levelname)-8s %(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stdout)
+        ],
+        level=args.loglevel
+    )
+
+    logging.info("input file(s): {}".format(
+        [x.name for x in args.input_files]))
+    logging.info("output file: {}".format(args.output_file))
+    logging.info("certificate issue(s) to process: {}".format(
+        sorted(certificates.keys())))
+    logging.info("protocol(s) to process: {}".format(protocols))
+    logging.info("vulnerability/ies to process: {}".format(
+        sorted(vulnerabilities.keys())))
+
+    data = []
+
+    for file in args.input_files:
+        data.append(json.load(file))
+
+    workbook = xlsxwriter.Workbook("{}".format(args.output_file))
+
+    logging.info("generating worksheet 'Host vs Certificate'...")
+    parse_host_certificate(workbook, data)
+
+    logging.info("generating worksheet 'Host vs Certificates'...")
+    parse_host_certificates(workbook, data)
+
+    logging.info("generating worksheet 'Host vs Protocol'...")
+    parse_host_protocol(workbook, data)
+
+    logging.info("generating worksheet 'Host vs Protocols'...")
+    parse_host_protocols(workbook, data)
+
+    logging.info("generating worksheet 'Host vs Ciphers'...")
+    parse_host_ciphers(workbook, data)
+
+    logging.info("generating worksheet 'Host vs Vulnerability'...")
+    parse_host_vulnerability(workbook, data)
+
+    logging.info("generating worksheet 'Host vs Vulnerabilities'...")
+    parse_host_vulnerabilities(workbook, data)
+
+    workbook.close()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        logging.exception("{}".format(e))
